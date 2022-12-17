af1cda1fff9b0d3aad8b7a843f24c4e233007304
dicenull <dicenull.dev@gmail.com>
2022-12-18 05:15:16 +0900

    👍 仮データで選択UI表示

--- lib/feature_yolo/ui/main_page.dart
+++ lib/feature_yolo/ui/main_page.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:hackathon2022_team_jig/data/toast_state.dart';
 import 'package:hackathon2022_team_jig/feature_yolo//data/entity/recognition.dart';
 import 'package:hackathon2022_team_jig/feature_yolo//data/model/ml_camera.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -93,17 +95,25 @@ class MainPage extends HookConsumerWidget {
     final recognitions = ref.watch(recognitionsProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter YOLOv5 App'),
+        title: const Text('ゴミ分別'),
       ),
       body: mlCamera.when(
-        data: (mlCamera) => Stack(
+        data: (mlCamera) => Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            CameraView(cameraController: mlCamera.cameraController),
-            buildBoxes(
-              recognitions,
-              mlCamera.actualPreviewSize,
-              mlCamera.ratio,
+            Stack(
+              children: [
+                CameraView(cameraController: mlCamera.cameraController),
+                buildBoxes(
+                  recognitions,
+                  mlCamera.actualPreviewSize,
+                  mlCamera.ratio,
+                ),
+              ],
             ),
+            const Flexible(
+              child: _ToastCategories(),
+            )
           ],
         ),
         loading: () => const Center(
@@ -134,6 +144,128 @@ class MainPage extends HookConsumerWidget {
           ratio: ratio,
         );
       }).toList(),
+    );
+  }
+}
+
+class _Badge extends StatelessWidget {
+  final ToastState data;
+
+  const _Badge({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.cyan,
+          width: 2,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        data.separation,
+        style: const TextStyle(
+          fontSize: 24,
+          height: 1.2,
+        ),
+      ),
+    );
+  }
+}
+
+class _Toast extends StatelessWidget {
+  final ToastState data;
+
+  const _Toast(
+    this.data, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 32,
+        vertical: 8,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CachedNetworkImage(
+            imageUrl: data.icon,
+            width: 64,
+            height: 64,
+          ),
+          Text(
+            data.item,
+            style: const TextStyle(
+              fontSize: 32,
+              height: 1.2,
+            ),
+          ),
+          const SizedBox(width: 8),
+          _Badge(data: data),
+        ],
+      ),
+    );
+  }
+}
+
+class _ToastCategories extends HookConsumerWidget {
+  const _ToastCategories({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final list = ref.watch(toastProvider);
+    final list = [
+      ToastState(
+        icon:
+            "https://images.microcms-assets.io/assets/42f2dca8f920452c91f3af77ab894c44/15c048386e9e494ea7573c69e75f9a7b/icon1.png",
+        item: "衣類",
+        category: "資源物",
+        separation: "繊維類",
+      ),
+      ToastState(
+        icon:
+            "https://images.microcms-assets.io/assets/42f2dca8f920452c91f3af77ab894c44/15c048386e9e494ea7573c69e75f9a7b/icon1.png",
+        item: "衣類",
+        category: "資源物",
+        separation: "繊維類",
+      ),
+      ToastState(
+        icon:
+            "https://images.microcms-assets.io/assets/42f2dca8f920452c91f3af77ab894c44/15c048386e9e494ea7573c69e75f9a7b/icon1.png",
+        item: "衣類",
+        category: "資源物",
+        separation: "繊維類",
+      ),
+      ToastState(
+        icon:
+            "https://images.microcms-assets.io/assets/42f2dca8f920452c91f3af77ab894c44/15c048386e9e494ea7573c69e75f9a7b/icon1.png",
+        item: "衣類",
+        category: "資源物",
+        separation: "繊維類",
+      ),
+      ToastState(
+        icon:
+            "https://images.microcms-assets.io/assets/42f2dca8f920452c91f3af77ab894c44/15c048386e9e494ea7573c69e75f9a7b/icon1.png",
+        item: "衣類",
+        category: "資源物",
+        separation: "繊維類",
+      ),
+    ];
+
+    return ListView(
+      children: [
+        ...list.map((toast) => _Toast(toast)),
+      ],
     );
   }
 }
