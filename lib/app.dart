3831e7e30ebc13f5229b4cec938adadd1fc64cc9
Takeru Miyake <m18kecam777@gmail.com>
2022-12-18 04:37:11 +0900

    👍 アプリ開いたと同時にmicroCMSのデータをfetchするようにした

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hackathon2022_team_jig/feature_yolo/ui/main_page.dart';
+import 'package:hackathon2022_team_jig/model/separation_controller.dart';
+import 'package:hackathon2022_team_jig/model/yoko_controller.dart';
+import 'package:hackathon2022_team_jig/util/use_async_effect.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class App extends HookWidget {
+class App extends HookConsumerWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(separationProvider);
+    ref.watch(yoloProvider);
+
+    useAsyncEffect(() async {
+      await ref.read(separationProvider.notifier).fetch();
+      await ref.read(yoloProvider.notifier).fetch();
+    }, const []);
+
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       title: 'ごみ分別',
