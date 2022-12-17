49829c543d152c46d625d08961f949245e7e9e44
Takeru Miyake <m18kecam777@gmail.com>
2022-12-18 07:21:59 +0900

    👍 microCMSのデータをfetch

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hackathon2022_team_jig/feature_yolo/data/model/ml_camera.dart';
 import 'package:hackathon2022_team_jig/feature_yolo/ui/main_page.dart';
 import 'package:hackathon2022_team_jig/model/separation_controller.dart';
+import 'package:hackathon2022_team_jig/model/toast_controller.dart';
 import 'package:hackathon2022_team_jig/model/yoko_controller.dart';
 import 'package:hackathon2022_team_jig/util/use_async_effect.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,12 +15,31 @@ class App extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(separationProvider);
-    ref.watch(yoloProvider);
-
+    final yoloState = ref.watch(yoloProvider);
+    ref.watch(toastProvider);
+    ref
+        .watch(recognitionsProvider)
+        .sort(((a, b) => -a.score.compareTo(b.score)));
+    final a = ref.watch(recognitionsProvider);
+    final highLabel = a.isEmpty ? '' : a[0].displayLabel;
     useAsyncEffect(() async {
+      await dotenv.load(fileName: '.env');
       await ref.read(separationProvider.notifier).fetch();
       await ref.read(yoloProvider.notifier).fetch();
+      return;
     }, const []);
+
+    useAsyncEffect(() async {
+      ref.read(toastProvider.notifier).updateToastMessage('bottle');
+      return;
+    }, [yoloState]);
+
+    useEffect(() {
+      if (highLabel.isNotEmpty) {
+        ref.read(toastProvider.notifier).updateToastMessage(highLabel);
+      }
+      return;
+    }, [highLabel]);
 
     return MaterialApp(
       debugShowCheckedModeBanner: true,
